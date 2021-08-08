f559dade6d47a0b96c6a514b0723109eb7a8ce0e
Marcos <marcosnicolau2003@gmailcom>
2021-08-08 11:52:37 -0300

    feat: automated certificate
    Added automated certificate, now there is no prompt

--- scripts/deploy/certificate.bash
+++ scripts/deploy/certificate.bash
@@ -4,7 +4,9 @@ echo GETTING CERTIFICATE...
 sudo snap install core
 sudo snap install --classic certbot
 sudo ln -s /snap/bin/certbot /usr/bin/certbot
-echo y | sudo certbot --nginx --agree-tos -m $EMAIL
+# REPLACE WHITE SPACES FOR COMMA
+DOMAINS_CERTBOT_SINTAX=$( echo $DOMAIN | sed 's/ /,/g' )
+sudo certbot --nginx --agree-tos -m $EMAIL --hsts --uir -n -d $DOMAINS_CERTBOT_SINTAX
 
 # AUTOMATIC CERTIFICATE RENEWAL
 echo ===================================================================================================
