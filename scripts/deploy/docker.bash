0f5a2a1d2635b4200283ce373acd9ff18ace23f8
Marcos <marcosnicolau2003@gmailcom>
2021-08-08 11:55:55 -0300

    feat: :bug: port

--- scripts/deploy/docker.bash
+++ scripts/deploy/docker.bash
@@ -7,17 +7,18 @@ echo "
 # DOCKER LOGIN
 if [ -z $DOCKER_USERNAME ]
 then 
-    # DOCKER RUN
+    # PUBLIC REPOSITORY
     echo ===================================================================================================
     echo RUNNING IMAGE...
-    docker run --name app --env-file ~/app/.docker.env -p 80:$DOCKER_EXPOSE_PORT -d $DOCKER_IMAGE_NAME
+    docker run --name app --env-file ~/app/.docker.env -p 8080:$DOCKER_EXPOSE_PORT -d $DOCKER_IMAGE_NAME
 else 
+    # PRIVATE REPOSITORY, WE NEED TO LOGIN
     echo ===================================================================================================
     echo LOGING INTO DOCKER..
     docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 
     ==================================================================================================
     echo RUNNING IMAGE...
-    docker run --name app --env-file ~/app/.docker.env -p 80:$DOCKER_EXPOSE_PORT -d $DOCKER_IMAGE_NAME
+    docker run --name app --env-file ~/app/.docker.env -p 8080:$DOCKER_EXPOSE_PORT -d $DOCKER_IMAGE_NAME
 fi
 
