c042928966c2678b3314add58abd85c24211f6c5
Marcos <marcosnicolau2003@gmailcom>
2021-08-08 11:53:09 -0300

    refactor: :art: refactor

--- scripts/deploy/pipeline.bash
+++ scripts/deploy/pipeline.bash
@@ -1,43 +1,27 @@
 # CD
-echo "NOW LET'S SET UP A CD PIPELINE"
-if [-z $DEPLOY_TOKEN] ; then return
+if [ -z $PIPELINE_TOKEN]  
+then 
+        echo YESSSS EVERYTHING WAS DONE CORRECTLY, CONGRATULATIONS '🎉🎉🎉🎉🎉' 
+        exit
 fi
-if [-z $PIPELINE_CONTAINER_PORT] ; then PIPELINE_CONTAINER_PORT=3000
-fi
-echo deploy.sh "
-DEPLOY_SUCCESS=""
-source ~/app/.deploy.env
-source ~/app/.docker.env
-if [ -z $DOCKER_USERNAME ]
-then
-        MESSAGE=(docker pull $DOCKER_IMAGE_NAME && \
-        docker stop app  && \
-        docker rm  app && \
-        docker run --name app --env-file ~/app/.docker.env -p 80:$DOCKER_EXPOSE_PORT -d $DOCKER_IMAGE_NAME && \
-        DEPLOY_SUCCESS=true 2>&1)
-        curl http://host.docker.internal:$PIPELINE_CONTAINER_PORT/mail/deploy?status=$DEPLOY_SUCCESS&log=$MESSAGE
-else 
-        MESSAGE=(docker logout && \
-        docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD && \
-        docker pull $DOCKER_IMAGE_NAME && \
-        docker stop app  && \
-        docker rm  app && \
-        docker run --name app --env-file ~/app/.docker.env -p 80:$DOCKER_EXPOSE_PORT -d $DOCKER_IMAGE_NAME && \
-        curl http://host.docker.internal:$PIPELINE_CONTAINER_PORT/mail/deploy?status=$DEPLOY_SUCCESS&log=$MESSAGE
-fi
-" > /scripts/deploy.sh
 
-chmod +x /scripts/deploy.sh
+echo ===================================================================================================
+echo SETTING PIPELINE...
+if [ -z $PIPELINE_CONTAINER_PORT] ; then PIPELINE_CONTAINER_PORT=8880
+fi
+
+chmod +x ./create-auto-pull.bash
+./create-auto-pull.bash
 
 echo ===================================================================================================
 echo PULLING PIPELINE IMAGE FROM DOCKER...
-docker run \ 
-        --add-host host.docker.internal:host-gateway \
+docker run  -d \
         -p $PIPELINE_CONTAINER_PORT:3000 \
+        --add-host host.docker.internal:host-gateway \
         -v /var/run/docker.sock:/var/run/docker.sock \
         -v /path/to/static-docker-binary:/usr/bin/docker \
         -v /scripts:/scripts \ 
-        --env-file /app/.deploy.env \
+        --env-file ~/app/.deploy.env \
         marcosnicolau/webhook-listener
 
 
