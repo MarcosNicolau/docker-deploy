d8e2c7e7cac0e2bd26b8fae804a6a6607beeaf47
Marcos <marcosnicolau2003@gmailcom>
2021-08-08 11:55:05 -0300

    feat: nginx redirect to pipeline port on deploy route

--- scripts/deploy/nginx.bash
+++ scripts/deploy/nginx.bash
@@ -14,11 +14,23 @@ sudo systemctl enable nginx
 echo ===================================================================================================
 echo CREATING NGINX SERVER BASED ON DOMAIN...
 sudo rm /etc/nginx/sites-available/default
+
 sudo echo "server {
     listen 80;
     server_name $DOMAIN;
     location / {
         proxy_pass http://127.0.0.1:8080;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection 'upgrade';
+        proxy_set_header Host \$host;
+        proxy_cache_bypass \$http_upgrade;
+        proxy_redirect off;
+     }
+
+   # Redirect to pipeline port
+     location $PIPELINE_DEPLOY_ROUTE {
+        proxy_pass http://127.0.0.1:$PIPELINE_CONTAINER_PORT;
         proxy_http_version 1.1;
         proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection 'upgrade';
