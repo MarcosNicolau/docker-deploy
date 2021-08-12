293e8cced9a739907f824fbcada4e8bee2e3caa8
Marcos <marcosnicolau2003@gmailcom>
2021-08-11 22:44:19 -0300

    refactor: cp auto-deploy
    now cp auto deploy file instead of writing it in here

--- scripts/deploy/pipeline.bash
+++ scripts/deploy/pipeline.bash
@@ -1,3 +1,5 @@
+source $PATH/.deploy.env
+
 # CD
 if [ -z $PIPELINE_TOKEN]; then 
         echo YESSSS EVERYTHING WAS DONE CORRECTLY, CONGRATULATIONS '🎉🎉🎉🎉🎉' 
@@ -6,20 +8,18 @@ fi
 
 echo ===================================================================================================
 echo SETTING PIPELINE...
-if [ -z $PIPELINE_CONTAINER_PORT] ; then PIPELINE_CONTAINER_PORT=8880
-fi
-
-chmod +x ./create-auto-pull.bash
-./create-auto-pull.bash
+cp $SCRIPTS_PATH/auto-deploy.bash /scripts/deploy.sh 
+cp $PATH/.deploy.env /scripts/.deploy.env 
+cp $PATH/.deploy.env /scripts/.docker.env 
 
 echo ===================================================================================================
 echo PULLING PIPELINE IMAGE FROM DOCKER...
 docker run  -d -p $PIPELINE_CONTAINER_PORT:3000 \
         --add-host host.docker.internal:host-gateway \
         -v /var/run/docker.sock:/var/run/docker.sock \
-        -v /path/to/static-docker-binary:/usr/bin/docker \
-        -v ~/scripts:~/scripts \
-        --env-file ~/app/.deploy.env \
+        -v /scripts:/scripts \
+        --env-file $PATH/.deploy.env \
+        --name webhook \
         marcosnicolau/webhook-listener
 
 
