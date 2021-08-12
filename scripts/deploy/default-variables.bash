31e89d077a7675556eef2407dd2cc0265ee82020
Marcos <marcosnicolau2003@gmailcom>
2021-08-12 13:47:21 -0300

    fix: remove source
    remove source on file writing

--- scripts/deploy/default-variables.bash
+++ scripts/deploy/default-variables.bash
@@ -1,11 +1,11 @@
 PATH_TO_DIR=~/docker-deploy
 source $PATH_TO_DIR/.deploy.env
 
-if [ -z $PIPELINE_DEPLOY_ROUTE ]; then echo 'PIPELINE_DEPLOY_ROUTE=/deploy' >> source ~/docker-deploy/.deploy.env
+if [ -z $PIPELINE_DEPLOY_ROUTE ]; then echo 'PIPELINE_DEPLOY_ROUTE=/deploy' >> $PATH_TO_DIR/.deploy.env
 fi
 
-if [ -z $PIPELINE_CONTAINER_PORT ]; then echo 'PIPELINE_CONTAINER_PORT=8880' >> source ~/docker-deploy/.deploy.env
+if [ -z $PIPELINE_CONTAINER_PORT ]; then echo 'PIPELINE_CONTAINER_PORT=8880' >> $PATH_TO_DIR/.deploy.env
 fi
 
-if [ -z $DOCKER_PORT ]; then echo 'DOCKER_PORT=8080' >> source ~/docker-deploy/.deploy.env
+if [ -z $DOCKER_PORT ]; then echo 'DOCKER_PORT=8080' >> $PATH_TO_DIR/.deploy.env
 fi
